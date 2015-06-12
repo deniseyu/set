a654466d8a8970e9b605c641d827330e1b7d7b49
Spike Nox <spike01@gmail.com>
2015-06-12 10:41:47 +0100

    Tiny refactor

--- lib/set_cards.rb
+++ lib/set_cards.rb
@@ -1,38 +1,48 @@
 class SetCards
 
-  NUMBERS = [1, 2, 3]
-  COLOURS = [:red, :purple, :green]
-  FILLS = [:solid, :stripe, :empty]
-  SHAPES = [:diamond, :pill, :squiggle]
+  NUMBERS = [1, 2, 3].freeze
+  COLOURS = [:red, :purple, :green].freeze
+  FILLS = [:solid, :stripe, :empty].freeze
+  SHAPES = [:diamond, :pill, :squiggle].freeze
 
-  CARDS = NUMBERS.map do |number|
-    COLOURS.map do |colour|
-      FILLS.map do |fill|
-        SHAPES.map do |shape|
-          {
-            number: number,
-            colour: colour,
-            fill: fill,
-            shape: shape
-          }
+  def self.create_cards
+    NUMBERS.map do |number|
+      COLOURS.map do |colour|
+        FILLS.map do |fill|
+          SHAPES.map do |shape|
+            {
+              number: number,
+              colour: colour,
+              fill: fill,
+              shape: shape
+            }
+          end
         end
       end
-    end
-  end.flatten(3)
+    end.flatten(3)
+  end
+
+  CARDS = create_cards.freeze
 
   def all_triples
     CARDS.combination(3).to_a
   end
 
-  def valid_sets?(combination)
+  def valid_set?(combination)
     combination.map(&:values).transpose.select do |attribute|
       attribute.uniq.length == 2
     end.empty?
   end
 
   def count_valid_sets(all_selections)
+    valid_sets(all_selections).count(true)
+  end
+
+  private
+
+  def valid_sets(all_selections)
     all_selections.map do |selection|
-      valid_sets?(selection)
-    end.count(true)
+      valid_set?(selection)
+    end
   end
 end
--- spec/set_cards_spec.rb
+++ spec/set_cards_spec.rb
@@ -6,31 +6,40 @@ describe SetCards do
     expect(subject.all_triples.length).to eq 85320
   end
 
-  it 'checks if a combination has two cards with the same attribute' do
+  it 'does not validate a combination that has two cards with the same attribute' do
     combination = [
       {:number=>2, :colour=>:red, :fill=>:solid, :shape=>:pill},
       {:number=>2, :colour=>:purple, :fill=>:solid, :shape=>:squiggle},
       {:number=>3, :colour=>:green, :fill=>:stripe, :shape=>:pill}
     ]
-    expect(subject.valid_sets?(combination)).to be false
+    expect(subject.valid_set?(combination)).to be_falsey
   end
 
-  it 'checks if a combination of all distinct cards is a valid set' do
+  it 'validates a combination of all distinct cards' do
     combination = [
       {:number=>1, :colour=>:red, :fill=>:solid, :shape=>:pill},
       {:number=>2, :colour=>:purple, :fill=>:empty, :shape=>:squiggle},
       {:number=>3, :colour=>:green, :fill=>:stripe, :shape=>:diamond}
     ]
-    expect(subject.valid_sets?(combination)).to be true
+    expect(subject.valid_set?(combination)).to be_truthy
   end
 
-  it 'checks if all rook same' do
+  it 'validates a combination of cards with a single shared attribute' do
     combination = [
       {:number=>1, :colour=>:red, :fill=>:solid, :shape=>:pill},
       {:number=>1, :colour=>:purple, :fill=>:empty, :shape=>:squiggle},
       {:number=>1, :colour=>:green, :fill=>:stripe, :shape=>:diamond}
     ]
-    expect(subject.valid_sets?(combination)).to be true
+    expect(subject.valid_set?(combination)).to be_truthy
+  end
+
+  it 'validates a combination of cards with a two shared attribute' do
+    combination = [
+      {:number=>1, :colour=>:red, :fill=>:solid, :shape=>:pill},
+      {:number=>1, :colour=>:red, :fill=>:empty, :shape=>:squiggle},
+      {:number=>1, :colour=>:red, :fill=>:stripe, :shape=>:diamond}
+    ]
+    expect(subject.valid_set?(combination)).to be_truthy
   end
 
   it 'finds all valid sets' do
