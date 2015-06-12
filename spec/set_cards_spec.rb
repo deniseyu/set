0bfacc270167dc2dee174d77d96f29ee39c94aff
Spike Nox <spike01@gmail.com>
2015-06-12 10:44:16 +0100

    Named subject

--- spec/set_cards_spec.rb
+++ spec/set_cards_spec.rb
@@ -2,8 +2,10 @@ require 'set_cards'
 
 describe SetCards do
 
+  subject(:set_cards) { described_class.new }
+
   it 'generates all possible combinations of 3 cards' do
-    expect(subject.all_triples.length).to eq 85320
+    expect(set_cards.all_triples.length).to eq 85320
   end
 
   it 'does not validate a combination that has two cards with the same attribute' do
@@ -12,7 +14,7 @@ describe SetCards do
       {:number=>2, :colour=>:purple, :fill=>:solid, :shape=>:squiggle},
       {:number=>3, :colour=>:green, :fill=>:stripe, :shape=>:pill}
     ]
-    expect(subject.valid_set?(combination)).to be_falsey
+    expect(set_cards.valid_set?(combination)).to be_falsey
   end
 
   it 'validates a combination of all distinct cards' do
@@ -21,7 +23,7 @@ describe SetCards do
       {:number=>2, :colour=>:purple, :fill=>:empty, :shape=>:squiggle},
       {:number=>3, :colour=>:green, :fill=>:stripe, :shape=>:diamond}
     ]
-    expect(subject.valid_set?(combination)).to be_truthy
+    expect(set_cards.valid_set?(combination)).to be_truthy
   end
 
   it 'validates a combination of cards with a single shared attribute' do
@@ -30,7 +32,7 @@ describe SetCards do
       {:number=>1, :colour=>:purple, :fill=>:empty, :shape=>:squiggle},
       {:number=>1, :colour=>:green, :fill=>:stripe, :shape=>:diamond}
     ]
-    expect(subject.valid_set?(combination)).to be_truthy
+    expect(set_cards.valid_set?(combination)).to be_truthy
   end
 
   it 'validates a combination of cards with a two shared attribute' do
@@ -39,10 +41,10 @@ describe SetCards do
       {:number=>1, :colour=>:red, :fill=>:empty, :shape=>:squiggle},
       {:number=>1, :colour=>:red, :fill=>:stripe, :shape=>:diamond}
     ]
-    expect(subject.valid_set?(combination)).to be_truthy
+    expect(set_cards.valid_set?(combination)).to be_truthy
   end
 
   it 'finds all valid sets' do
-    expect(subject.count_valid_sets(subject.all_triples)).to eq 1080
+    expect(set_cards.count_valid_sets(subject.all_triples)).to eq 1080
   end
 end
